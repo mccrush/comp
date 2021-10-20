1754f7fd4ab00afae72e2b0e55ffcdfcab5ab4ad
mccrush <mccrush2027@gmail.com>
2021-10-21 00:20:18 +0700

    Add config

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// vue.config.js
+module.exports = {
+  productionSourceMap: false,
+  publicPath: '/comp/', // Путь в подкаталог для домена Github page
+  outputDir: 'docs' // Имя директории для сборки, для Github page
+}
