ec0c1f05429077fcb82b85c1810c5b5877fdcf7c
mccrush <mccrush2027@gmail.com>
2021-10-21 11:41:48 +0700

    Build proj

---
+++ docs/precache-manifest.1ea69ee644e7d94a1bc02f72f1488933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484005d69cfd0bd56bfd",
+    "url": "/comp/css/app.aaa874f9.css"
+  },
+  {
+    "revision": "9311e0f55b7deab8af59",
+    "url": "/comp/css/chunk-vendors.5d3035bc.css"
+  },
+  {
+    "revision": "e7a26eb6dbb8cf263c80ff4e2eac05bf",
+    "url": "/comp/index.html"
+  },
+  {
+    "revision": "484005d69cfd0bd56bfd",
+    "url": "/comp/js/app.69abfd82.js"
+  },
+  {
+    "revision": "9311e0f55b7deab8af59",
+    "url": "/comp/js/chunk-vendors.bf677c15.js"
+  },
+  {
+    "revision": "643d96b9781f4cdb37420fde162d244d",
+    "url": "/comp/manifest.json"
+  }
+]);
