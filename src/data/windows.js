1cae8b4f21011e63e956d73698d3d74a62d4ee4e
mccrush <mccrush2027@gmail.com>
2021-10-27 21:55:35 +0700

    Add comand

--- src/data/windows.js
+++ src/data/windows.js
@@ -1,7 +1,7 @@
 export default [
   { title: 'Проверить резервные файлы на вирусы', alias: 'viruscheck', check: false },
   { title: 'Создать резервную копию файлов', alias: 'copyfiles', check: false },
-  { title: 'Записать модель ПК и разрядность ЦП (dxdiag)', alias: 'sisteminfo', check: false },
+  { title: 'Записать модель ПК и разрядность ЦП (msinfo32, dxdiag)', alias: 'sisteminfo', check: false },
   { title: 'Скачать драйвера (сетевые) на флешку ', alias: 'drivers', check: false },
   { title: 'Сохранить параметры сетевых настроек', alias: 'networksettings', check: false },
   { title: 'Отключить автообновление win 7', alias: 'offupdate', check: false },
